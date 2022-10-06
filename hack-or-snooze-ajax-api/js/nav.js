fb4922de47133f1ad55b6ccbec8297215d4b8cf9
clchen-arcadia <clchen.arcadia@gmail.com>
2022-10-06 17:54:06 -0400

    Do some work on Part 2B with submit form handling
    Added CSS "overlay" for the submit new story form.
    Added button to bring up submit new story form.
    Submit new story form has three fields: Title, Author, Story URL.
    At the bottom of the form is a submit button and a close button that will close the overlay.

--- hack-or-snooze-ajax-api/js/nav.js
+++ hack-or-snooze-ajax-api/js/nav.js
@@ -36,3 +36,10 @@ function updateNavOnLogin() {
   $navLogOut.show();
   $navUserProfile.text(`${currentUser.username}`).show();
 }
+
+function navShowSubmitForm(){
+  console.debug("navShowSubmitForm");
+  $(".submit-new-story-container").show();
+}
+
+$navSubmit.on("click", navShowSubmitForm);
